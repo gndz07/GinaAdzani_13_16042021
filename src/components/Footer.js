37cadeebf48a60400cd845229e6082a6a17b9c52
Gina A <ginadzani@gmail.com>
2021-04-15 20:35:53 +0200

    Fix: class into className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import '../styles/footer.css'
 export default class Footer extends React.Component {
 	render () {
 		return (
-			<footer class="footer">
-		      <p class="footer-text">Copyright {(new Date().getFullYear())} Argent Bank</p>
+			<footer className="footer">
+		      <p className="footer-text">Copyright {(new Date().getFullYear())} Argent Bank</p>
 		    </footer>
 		)
 	}
