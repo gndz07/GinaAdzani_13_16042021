139533a6379aa6dcbdd86d2791de5a5bb8813580
Gina A <ginadzani@gmail.com>
2021-04-15 20:33:57 +0200

    Feat: Add react router

--- src/apps/Router.js
+++ src/apps/Router.js
@@ -1,3 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '../components/Header.js';
+import Footer from '../components/Footer.js';
+import Homepage from '../pages/Homepage.js';
+import SignIn from '../pages/SignIn.js';
+import '../styles/root.css'
 
+export default class PageRouter extends React.Component {
+	render () {
+		return (
+			<Router>
+		    	<div id="page-body">
+				    <Header />
+				    <Switch>
+				    	<Route exact path='/' component={Homepage} />
+				    	<Route path='/signin' component={SignIn} />
+				    </Switch>
+				    <Footer />
+				</div>
+		    </Router>
+		)
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Homepage from './pages/Homepage.js';
 import reportWebVitals from './reportWebVitals';
-import './styles/root.css'
+import PageRouter from './apps/Router.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Homepage />
+    <PageRouter />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
