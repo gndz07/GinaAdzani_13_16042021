9d619d46073cf8d4b60e714eee610bbb78baa303
Gina A <ginadzani@gmail.com>
2021-04-15 20:36:56 +0200

    Refactor: Change page components

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import Header from '../components/Header.js'
+
 import Hero from '../components/Hero.js'
 import Features from '../components/Features.js'
-import Footer from '../components/Footer.js'
+
 
 export default class Homepage extends React.Component {
 	render () {
 		return (
-			<div>
-				<Header />
+			<div>	
 				<Hero />
 				<Features />
-				<Footer />
 			</div>
 		)
 	}
