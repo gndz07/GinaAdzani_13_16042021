c941fc9e04426aab565b897b20dca892ff452e40
Gina A <ginadzani@gmail.com>
2021-04-21 19:21:06 +0200

    Fix: header for logged in user

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import { connect } from 'react-redux';
 import Logo from '../assets/argentBankLogo.png'
 import '../styles/header.css'
 
-export default class Header extends React.Component {
+class Header extends React.Component {
 	render () {
+		const guestHeader = 
+			<div>
+		        <NavLink to='/login' className="main-nav-item">
+		          <i className="fa fa-user-circle"></i>
+		          Sign In
+		        </NavLink>
+		    </div>
+
+		const loggedInHeader =  
+			<div>
+		        <NavLink to='/user/profile' className="main-nav-item">
+		          <i className="fa fa-user-circle"></i>
+		          {this.props.user.user.firstName}
+		        </NavLink>
+
+		         <NavLink to='/user/profile' className="main-nav-item">
+		          <i className="fa fa-sign-out"></i>
+		          Sign Out
+		        </NavLink>
+		    </div>
+
 		return (
 			<nav className="main-nav">
 		      <NavLink to='/' exact={true} className="main-nav-logo">
@@ -12,13 +34,14 @@ export default class Header extends React.Component {
 		        <h1 className="sr-only">Argent Bank</h1>
 		      </NavLink>
 
-		      <div>
-		        <NavLink to='/login' className="main-nav-item">
-		          <i className="fa fa-user-circle"></i>
-		          Sign In
-		        </NavLink>
-		      </div>
+		     {this.props.user.isLoggedIn ? loggedInHeader : guestHeader}
 		    </nav>
 		)
 	}
 }
+
+const mapStateToProps = state => ({
+	user: state.login
+});
+
+export default connect(mapStateToProps)(Header);
